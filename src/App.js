3af3ed286057b2c418b7dd2fc8a4d5bcab45893a
Samagra Agrawal <samagra110@gmail.com>
2024-02-10 20:11:32 +0530

    Create About Component and add dark theme option

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm.js';
+import About from './components/About.js';
 
 function App() {
   return (
     <>
       <Navbar title="Textutils" aboutText="About Textutils" />
       <TextForm></TextForm>
+      {/* <About></About> */}
     </>
   );
 }
